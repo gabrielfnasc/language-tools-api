77b7079d3fd3d76e047904415e52484720827096
GabrielfNascimento <gf31956@gmail.com>
2022-11-09 19:16:34 -0300

    changed name Rword to RwordAdapter

--- src/infrastructure/adapters/RwordAdapter.ts
+++ src/infrastructure/adapters/RwordAdapter.ts
@@ -1,4 +1,4 @@
-import { RandomWord } from "../application/random-words/RandomWord";
+import { RandomWord } from "../../application/random-words/RandomWord";
 import { rword } from "rword";
 
 export class Rword implements RandomWord {
