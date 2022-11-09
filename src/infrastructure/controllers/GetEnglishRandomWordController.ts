25e670958a021bf607e05e46ba2a06352afee80c
GabrielfNascimento <gf31956@gmail.com>
2022-11-08 21:55:09 -0300

    implemented Controller

--- src/infrastructure/controllers/GetEnglishRandomWordController.ts
+++ src/infrastructure/controllers/GetEnglishRandomWordController.ts
@@ -1,11 +1,17 @@
+import { GetEnglishRandomWordUseCase } from "../../application/usecase/GetEnglishWordUseCase";
 import { HttpResponse } from "../http/presentation/controllers/helpers/Http";
-import { serverError } from "../http/presentation/controllers/helpers/HttpHelper";
+import {
+  ok,
+  serverError,
+} from "../http/presentation/controllers/helpers/HttpHelper";
 import { BaseController } from "./BaseController";
 
 export class GetEnglishRandomWordController implements BaseController {
-  constructor(private readonly usecase : ) {}
+  constructor(private readonly usecase: GetEnglishRandomWordUseCase) {}
   async handle(): Promise<HttpResponse> {
     try {
+      const output = await this.usecase.execute();
+      return ok(output);
     } catch (error) {
       return serverError(error as Error);
     }
