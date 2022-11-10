295238b3daecbe6a0257c537092544b9098fd0ff
GabrielfNascimento <gf31956@gmail.com>
2022-11-10 17:55:55 -0300

    created SentenceRepositoryMongoDb and save method

---
+++ src/infrastructure/database/mongodb/SentenceRepositoryMongoDb.ts
@@ -0,0 +1,15 @@
+import { SentenceRepository } from "../../../application/repositories/SentenceRepository";
+import { InputSaveSentenceDto } from "../../../application/usecase/SaveSentenceUseCase";
+import { BaseMongoRepository } from "./BaseMongoRepository";
+
+export class SentenceRepositoryMongoDb
+  extends BaseMongoRepository
+  implements SentenceRepository
+{
+  collection(): string {
+    return "sentences";
+  }
+  async save(data: InputSaveSentenceDto): Promise<void> {
+    await this.getCollection.insertOne({ data });
+  }
+}
