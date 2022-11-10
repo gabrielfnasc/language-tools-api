d2b51cd6d697d1fc30c0c8e80199c5b697a8cc1d
GabrielfNascimento <gf31956@gmail.com>
2022-11-10 17:58:06 -0300

    implemented SaveSentenceUseCase

--- src/application/usecase/SaveSentenceUseCase.ts
+++ src/application/usecase/SaveSentenceUseCase.ts
@@ -1,4 +1,5 @@
 import { Sentence } from "../../domain/entities/Sentence";
+import { SentenceRepository } from "../repositories/SentenceRepository";
 import { Usecase } from "./UseCase";
 
 export type InputSaveSentenceDto = {
@@ -8,7 +9,8 @@ export type InputSaveSentenceDto = {
 export class SaveSentenceUseCase
   implements Usecase<InputSaveSentenceDto, void>
 {
+  constructor(private readonly repository: SentenceRepository) {}
   async execute(data: InputSaveSentenceDto): Promise<void> {
-    throw new Error("Method not implemented.");
+    await this.repository.save(data);
   }
 }
