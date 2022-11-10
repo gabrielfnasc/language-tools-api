53febfa9dbf2ad9cafca63195e5a90129f0704f7
GabrielfNascimento <gf31956@gmail.com>
2022-11-10 18:21:21 -0300

    SaveSentenceFactory.ts

--- src/infrastructure/factories/SaveSentenceFactory.ts
+++ src/infrastructure/factories/SaveSentenceFactory.ts
@@ -1,10 +1,21 @@
+import { SaveSentenceUseCase } from "../../application/usecase/SaveSentenceUseCase";
+import { ValidatorComposite } from "../../application/validator/ValidatorComposite";
+import { ValidatorRequiredParam } from "../../application/validator/ValidatorRequiredParam";
 import { BaseController } from "../controllers/BaseController";
 import { SaveSentenceController } from "../controllers/SaveSentenceController";
+import { SentenceRepositoryMongoDb } from "../database/mongodb/SentenceRepositoryMongoDb";
 
 export class SaveSentenceFactory {
   static build(): BaseController {
-const validator = new 
+    const validatorRequest = new ValidatorComposite([
+      new ValidatorRequiredParam("content"),
+      new ValidatorRequiredParam("translation"),
+    ]);
 
-    return new SaveSentenceController();
+    const repository = new SentenceRepositoryMongoDb();
+
+    const usecase = new SaveSentenceUseCase(repository);
+
+    return new SaveSentenceController(validatorRequest, usecase);
   }
 }
